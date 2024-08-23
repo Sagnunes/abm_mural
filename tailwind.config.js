9fcc50d34b994e227ecb537049ce1817f1109290
Sérgio Nunes <sagnunes@gmail.com>
2024-08-23 14:51:30 +0100

    ended mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'title-gray': 'rgb(112, 112, 112)',
+                'gray-50w': 'rgb(119, 118, 109)',
+                'gray-75w': 'rgb(181, 181, 174)',
+                'gray-100w': 'rgb(147, 147, 147)'
+            }
+        },
+    },
+    plugins: [],
 }
