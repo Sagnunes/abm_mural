f8d6b08a124210e3646164fd6de269742d581387
Sérgio Nunes <sagnunes@gmail.com>
2024-08-23 15:20:24 +0100

    added hover colors to links

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
                 'title-gray': 'rgb(112, 112, 112)',
                 'gray-50w': 'rgb(119, 118, 109)',
                 'gray-75w': 'rgb(181, 181, 174)',
-                'gray-100w': 'rgb(147, 147, 147)'
+                'gray-100w': 'rgb(147, 147, 147)',
+                'gray-links':'#777777',
+                'gray-links-hover':'#B5B5AE'
             }
         },
     },
